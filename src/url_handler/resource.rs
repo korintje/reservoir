df5c7273ba83ed3015b543917a1d7caa8b9292ec
korintje <t.hosomi1410@gmail.com>
2021-12-29 13:00:26 +0900

    Fix tab number

--- src/url_handler/resource.rs
+++ src/url_handler/resource.rs
@@ -6,30 +6,30 @@ use response::{MyResponse};
 
 #[get("/resources/{id}")]
 async fn get_resource(resource_id: web::Path<i32>, accessor: web::Data<DataAccessor>) -> impl Responder {
-    let result = accessor.get_resource(resource_id.into_inner()).await;
-    match result {
-        Err(_) => MyResponse::item_not_found(),
-        Ok(resource) => HttpResponse::Ok().json(resource),
-    }
+  let result = accessor.get_resource(resource_id.into_inner()).await;
+  match result {
+    Err(_) => MyResponse::item_not_found(),
+    Ok(resource) => HttpResponse::Ok().json(resource),
+  }
 }
 
 #[get("/resources")]
 async fn get_resources(accessor: web::Data<DataAccessor>) -> impl Responder {
-    let result = accessor.get_resources().await;
-    match result {
-        Err(_) => MyResponse::item_not_found(),
-        Ok(resource) => HttpResponse::Ok().json(resource),
-    }
+  let result = accessor.get_resources().await;
+  match result {
+    Err(_) => MyResponse::item_not_found(),
+    Ok(resource) => HttpResponse::Ok().json(resource),
+  }
 }
 
 #[post("/resources")]
 async fn add_resource(resource: web::Json<Resource>, accessor: web::Data<DataAccessor>) -> impl Responder {
-    let resource = resource.into_inner();
-    let result = accessor.add_resource(resource).await;
-    match result {
-        Err(e) => MyResponse::bad_request(&e.to_string()),
-        Ok(_) => MyResponse::ok(),
-    }
+  let resource = resource.into_inner();
+  let result = accessor.add_resource(resource).await;
+  match result {
+    Err(e) => MyResponse::bad_request(&e.to_string()),
+    Ok(_) => MyResponse::ok(),
+  }
 }
 
 #[delete("/resources/{id}")]
@@ -41,8 +41,8 @@ async fn delete_resource(resource_id: web::Path<i32>, accessor: web::Data<DataAc
   }
   let result = accessor.delete_resource(resource_id).await;
   match result {
-      Err(_) => MyResponse::item_not_found(),
-      Ok(_) => MyResponse::ok(),
+    Err(_) => MyResponse::item_not_found(),
+    Ok(_) => MyResponse::ok(),
   }
 }
 
